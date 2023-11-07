3482aabddb5629840c991896c6adfe785397f72e
Denis Wachira <deniswachira77@gmail.com>
2023-11-07 23:08:13 +0300

    arrays

---
+++ arrayBasics/Program.cs
@@ -0,0 +1,24 @@
+﻿// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+//reassinging first element
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassigned First: {fraudulentOrderIDs[0]}");
+
+// length of array
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// iterating over array
+string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
